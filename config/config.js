4ffb8549910d9b5b2517b38b9c9db51012e0f323
gagan0024 <43841619+gagan0024@users.noreply.github.com>
2021-02-25 10:41:11 +0530

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,1 +1,10 @@
+const _ = require('lodash');
+
+const environment = process.env.NODE_ENV || 'development';
+
+const config = require(`../config/config.${environment}.json`);
+const defaultConfig = require(`../config/config.development.json`);
+const finalConfig = _.merge(defaultConfig, config);
+
+global.gConfig = finalConfig;
 
