183cdd86817b03020e3ce412b531ac7b769eb9b2
gagan0024 <43841619+gagan0024@users.noreply.github.com>
2021-02-25 10:43:07 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const {validateCreateTokenRequest} = require('../validators/createtokenrequest');
+  
 
+
+/**
+* @swagger
+* paths:
+*  /api/v1/auth/token:
+*    post:
+*      summary: Creates a new token.
+*      tags: 
+*           - token
+*      consumes:
+*        - application/json
+*      parameters:
+*        - in: body
+*          name: Token
+*          description: Create Token.
+*          schema:
+*            type: object
+*            required:
+*              - clientsecret
+*              - clientid
+*              - id 
+*            properties:
+*              clientsecret:
+*                type: string
+*              clientid:
+*                type: string
+*              id:
+*                type: string
+*      responses:
+*        200:
+*          description: Created
+*/          
+router.route('/token').post(validateCreateTokenRequest,authController.createToken);
+
+
+module.exports = router;
