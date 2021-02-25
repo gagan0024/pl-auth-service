9214c4bb5cd0a0da7554d6a650746b5d47935a7d
gagan0024 <43841619+gagan0024@users.noreply.github.com>
2021-02-25 10:42:13 +0530

    Add files via upload

--- controllerss/authController.js
+++ controllerss/authController.js
@@ -1,1 +1,23 @@
+const logger   = require('../components/utils/logger');
+const GenerateTokenProcess = require('../components/process/generateTokenProcess');
+
+let generateTokenProcess = new GenerateTokenProcess();
+
+exports.createToken = async (req, res) => {
+    try{
+        
+        let data = await generateTokenProcess.generateToken(req.body.clientsecret, req.body.clientid, req.body.id);
+        res.status(data[0]).json({
+            token:data[1]
+        });
+            
+    }
+    catch(err){
+        logger().error(err.toString());
+        res.status(500).json({
+            error:err.toString()
+        });
+    }
+};
+
 
